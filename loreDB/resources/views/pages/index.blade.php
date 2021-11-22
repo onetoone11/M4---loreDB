6aef69a908ce563c33f123204478ca12133add5d
Alexander Karlsson <alexander.karlsson@learnet.se>
2021-11-22 13:20:06 +0100

    did stuff to index

--- loreDB/resources/views/pages/index.blade.php
+++ loreDB/resources/views/pages/index.blade.php
@@ -14,10 +14,10 @@
                 <div class="section-item hover">
                     <h2>{{$universe->name}}</h2>
                     <div class="tags">
-                        <h3>sci-fi, genre 2, genre 3...</h3>
+                        <h3>{{$universe->leftJoin('uni_genres','universes.id','=','uni_genres.uni_id')->leftJoin('genres','uni_genres.genre_id','=','genres.id')->select('genres.name')->limit(3)->get()}}</h3>
                     </div>
                     <div class="tags">
-                        <h3>sci-fi, tag 2, tag 3...</h3>
+                        <h3>{{$universe->leftJoin('uni_tags','universes.id','=','uni_tags.uni_id')->leftJoin('tags','uni_tags.tag_id','=','tags.id')->select('tags.name')->limit(3)->get()}}</h3>
                     </div>
                     <div class="tags">
                         <p>created at: <br> {{$universe->created_at}}</>
